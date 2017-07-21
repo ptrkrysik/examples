239fca6c59ddc183e37df6efe3c6e4668032856a
Vasil Velichkov <vvvelichkov@gmail.com>
2017-07-22 01:45:25 +0300

    Regenerate uplink_decoding_multi_rtil with latest gr-gsm and gnuradio 3.7.10
    The prototype of clock_offset_control was changed and samp_rate now is
    required parameter.
    Rename ppm and ppm_in to ctrl and ctrl_in respectively

--- uplink_decoding_demo/uplink_decoding_multi_rtl.py
+++ uplink_decoding_demo/uplink_decoding_multi_rtl.py
@@ -5,7 +5,7 @@
 # Title: Uplink decoding demo
 # Author: Piotr Krysik
 # Description: Demonstration of uplink decoding using data captured with use of Multi-rtl
-# Generated: Mon May 23 16:48:53 2016
+# Generated: Fri Jul 21 22:41:12 2017
 ##################################################
 
 from gnuradio import blocks
@@ -20,8 +20,13 @@ import pmt
 
 class uplink_decoding_multi_rtl(gr.top_block):
 
-    def __init__(self):
+    def __init__(self, osr=4):
         gr.top_block.__init__(self, "Uplink decoding demo")
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.osr = osr
 
         ##################################################
         # Variables
@@ -52,16 +57,16 @@ class uplink_decoding_multi_rtl(gr.top_block):
         self.gsm_decryption_0_3 = grgsm.decryption(([0x46,0x77,0x2d,0x54,0xa3,0xa3,0xc3,0x1c]), 1)
         self.gsm_control_channels_decoder_0_1_0 = grgsm.control_channels_decoder()
         self.gsm_control_channels_decoder_0_1 = grgsm.control_channels_decoder()
-        self.gsm_clock_offset_control_0 = grgsm.clock_offset_control(947.8e6)
-        self.blocks_socket_pdu_0_0_0 = blocks.socket_pdu("UDP_CLIENT", "127.0.0.1", "4729", 10000, False)
-        self.blocks_file_source_0_0 = blocks.file_source(gr.sizeof_gr_complex*1, "sms_multirtl_uplink_tail", False)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, "sms_multirtl_downlink_tail", False)
+        self.gsm_clock_offset_control_0 = grgsm.clock_offset_control(947.8e6, samp_rate, osr)
+        self.blocks_socket_pdu_0_0_0 = blocks.socket_pdu("UDP_CLIENT", '127.0.0.1', '4729', 10000, False)
+        self.blocks_file_source_0_0 = blocks.file_source(gr.sizeof_gr_complex*1, 'sms_multirtl_uplink_tail.cfile', False)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, 'sms_multirtl_downlink_tail.cfile', False)
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.gsm_clock_offset_control_0, 'ppm'), (self.gsm_input_0, 'ppm_in'))    
-        self.msg_connect((self.gsm_clock_offset_control_0, 'ppm'), (self.gsm_input_0_0, 'ppm_in'))    
+        self.msg_connect((self.gsm_clock_offset_control_0, 'ctrl'), (self.gsm_input_0, 'ctrl_in'))    
+        self.msg_connect((self.gsm_clock_offset_control_0, 'ctrl'), (self.gsm_input_0_0, 'ctrl_in'))    
         self.msg_connect((self.gsm_control_channels_decoder_0_1, 'msgs'), (self.blocks_socket_pdu_0_0_0, 'pdus'))    
         self.msg_connect((self.gsm_control_channels_decoder_0_1, 'msgs'), (self.gsm_message_printer_0_0, 'msgs'))    
         self.msg_connect((self.gsm_control_channels_decoder_0_1_0, 'msgs'), (self.blocks_socket_pdu_0_0_0, 'pdus'))    
@@ -76,18 +81,35 @@ class uplink_decoding_multi_rtl(gr.top_block):
         self.connect((self.gsm_input_0, 0), (self.gsm_receiver_with_uplink_0, 0))    
         self.connect((self.gsm_input_0_0, 0), (self.gsm_receiver_with_uplink_0, 1))    
 
+    def get_osr(self):
+        return self.osr
+
+    def set_osr(self, osr):
+        self.osr = osr
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.gsm_input_0.set_samp_rate_in(self.samp_rate)
         self.gsm_input_0_0.set_samp_rate_in(self.samp_rate)
+        self.gsm_input_0.set_samp_rate_in(self.samp_rate)
+
+
+def argument_parser():
+    description = 'Demonstration of uplink decoding using data captured with use of Multi-rtl'
+    parser = OptionParser(usage="%prog: [options]", option_class=eng_option, description=description)
+    parser.add_option(
+        "", "--osr", dest="osr", type="intx", default=4,
+        help="Set OSR [default=%default]")
+    return parser
 
 
 def main(top_block_cls=uplink_decoding_multi_rtl, options=None):
+    if options is None:
+        options, _ = argument_parser().parse_args()
 
-    tb = top_block_cls()
+    tb = top_block_cls(osr=options.osr)
     tb.start()
     tb.wait()
 
