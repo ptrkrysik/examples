32727fc905c78b151cbb1794db05383220d7e61b
Piotr Krysik <ptrkrysik@gmail.com>
2017-12-01 12:03:36 +0100

    Added frequency offset correction to trx_radio_if

--- trx_radio_if/radio_if_grc.py
+++ trx_radio_if/radio_if_grc.py
@@ -5,9 +5,10 @@
 # Title: Trx radio interface
 # Author: (C) Piotr Krysik 2017
 # Description: Alpha version of trx radio interface
-# Generated: Sat Nov 11 11:20:15 2017
+# Generated: Fri Dec  1 10:55:18 2017
 ##################################################
 
+from change_sign_of_dict_elements import change_sign_of_dict_elements
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
@@ -23,24 +24,26 @@ import math
 import time
 
 
-class trx_radio_if(gr.top_block):
+class radio_if_grc(gr.top_block):
 
-    def __init__(self, delay_correction=285.616e-6, fc_downlink=938600000, fc_uplink=938900000, gain_downlink=40, gain_uplink=60, osr=4, ppm=-1, samp_rate=13e6/12.0, samp_rate_downlink=13e6/12.0, timing_advance=0):
+    def __init__(self, delay_correction=285.616e-6, osr=4, ppm=-0.799427, rx_freq=935e6+36*0.2e6, rx_gain=40, samp_rate=13e6/12.0, timing_advance=0, trx_base_port="5710", trx_remote_addr="127.0.0.1", tx_freq=935e6+36*0.2e6-45e6, tx_gain=40, uplink_shift=-(6.0/1625000*(156.25)*3)):
         gr.top_block.__init__(self, "Trx radio interface")
 
         ##################################################
         # Parameters
         ##################################################
         self.delay_correction = delay_correction
-        self.fc_downlink = fc_downlink
-        self.fc_uplink = fc_uplink
-        self.gain_downlink = gain_downlink
-        self.gain_uplink = gain_uplink
         self.osr = osr
         self.ppm = ppm
+        self.rx_freq = rx_freq
+        self.rx_gain = rx_gain
         self.samp_rate = samp_rate
-        self.samp_rate_downlink = samp_rate_downlink
         self.timing_advance = timing_advance
+        self.trx_base_port = trx_base_port
+        self.trx_remote_addr = trx_remote_addr
+        self.tx_freq = tx_freq
+        self.tx_gain = tx_gain
+        self.uplink_shift = uplink_shift
 
         ##################################################
         # Blocks
@@ -54,8 +57,8 @@ class trx_radio_if(gr.top_block):
         )
         self.uhd_usrp_source_0.set_clock_rate(26e6, uhd.ALL_MBOARDS)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_center_freq(fc_downlink, 0)
-        self.uhd_usrp_source_0.set_gain(gain_downlink, 0)
+        self.uhd_usrp_source_0.set_center_freq(rx_freq, 0)
+        self.uhd_usrp_source_0.set_gain(rx_gain, 0)
         self.uhd_usrp_source_0.set_antenna("RX2", 0)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
         	",".join(("", "")),
@@ -68,13 +71,14 @@ class trx_radio_if(gr.top_block):
         self.uhd_usrp_sink_0.set_clock_rate(26e6, uhd.ALL_MBOARDS)
         self.uhd_usrp_sink_0.set_subdev_spec("A:B", 0)
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_center_freq(fc_uplink, 0)
-        self.uhd_usrp_sink_0.set_gain(gain_uplink, 0)
+        self.uhd_usrp_sink_0.set_center_freq(tx_freq, 0)
+        self.uhd_usrp_sink_0.set_gain(tx_gain, 0)
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.ts_filter = grgsm.burst_timeslot_filter(0)
         self.low_pass_filter_0_0 = filter.fir_filter_ccf(1, firdes.low_pass(
         	1, samp_rate, 125e3, 5e3, firdes.WIN_HAMMING, 6.76))
-        self.gsm_txtime_setter_0 = grgsm.txtime_setter(None if (None is not None) else 0xffffffff, 0, 0, 0, 0, 0, 0)
-        self.gsm_trx_burst_if_0 = grgsm.trx_burst_if("127.0.0.1", "5700")
+        self.gsm_txtime_setter_0 = grgsm.txtime_setter(None if (None is not None) else 0xffffffff, 0, 0, 0, 0, timing_advance, delay_correction+uplink_shift)
+        self.gsm_trx_burst_if_0 = grgsm.trx_burst_if(trx_remote_addr, trx_base_port)
         self.gsm_receiver_0 = grgsm.receiver(4, ([0]), ([4]), False)
         self.gsm_preprocess_tx_burst_0 = grgsm.preprocess_tx_burst()
         self.gsm_msg_to_tag_0_0 = grgsm.msg_to_tag()
@@ -84,23 +88,30 @@ class trx_radio_if(gr.top_block):
             pulse_duration=4,
             sps=osr,
         )
-        self.gsm_controlled_rotator_cc_0_0 = grgsm.controlled_rotator_cc(-ppm/1.0e6*2*math.pi*fc_downlink/samp_rate)
-        self.gsm_controlled_rotator_cc_0 = grgsm.controlled_rotator_cc(ppm/1.0e6*2*math.pi*fc_downlink/samp_rate)
+        self.gsm_controlled_rotator_cc_0_0 = grgsm.controlled_rotator_cc(-ppm/1.0e6*2*math.pi*tx_freq/samp_rate)
+        self.gsm_controlled_rotator_cc_0 = grgsm.controlled_rotator_cc(ppm/1.0e6*2*math.pi*rx_freq/samp_rate)
+        self.gsm_clock_offset_control_0 = grgsm.clock_offset_control(rx_freq, samp_rate, osr)
         self.gsm_burst_type_filter_0 = grgsm.burst_type_filter(([3]))
         self.gsm_burst_to_fn_time_0 = grgsm.burst_to_fn_time()
         self.digital_burst_shaper_xx_0 = digital.burst_shaper_cc((firdes.window(firdes.WIN_HANN, 16, 0)), 0, 20, False, "packet_len")
+        self.change_sign_of_dict_elements = change_sign_of_dict_elements()
         self.blocks_pdu_to_tagged_stream_0_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, "packet_len")
 
         ##################################################
         # Connections
         ##################################################
+        self.msg_connect((self.change_sign_of_dict_elements, 'dict_out'), (self.gsm_msg_to_tag_0_0, 'msg'))    
         self.msg_connect((self.gsm_burst_to_fn_time_0, 'fn_time_out'), (self.gsm_txtime_setter_0, 'fn_time'))    
         self.msg_connect((self.gsm_burst_type_filter_0, 'bursts_out'), (self.gsm_burst_to_fn_time_0, 'bursts_in'))    
+        self.msg_connect((self.gsm_clock_offset_control_0, 'ctrl'), (self.change_sign_of_dict_elements, 'dict_in'))    
+        self.msg_connect((self.gsm_clock_offset_control_0, 'ctrl'), (self.gsm_msg_to_tag_0, 'msg'))    
         self.msg_connect((self.gsm_preprocess_tx_burst_0, 'bursts_out'), (self.blocks_pdu_to_tagged_stream_0_0, 'pdus'))    
         self.msg_connect((self.gsm_receiver_0, 'C0'), (self.gsm_burst_type_filter_0, 'bursts_in'))    
-        self.msg_connect((self.gsm_receiver_0, 'C0'), (self.gsm_trx_burst_if_0, 'bursts'))    
+        self.msg_connect((self.gsm_receiver_0, 'measurements'), (self.gsm_clock_offset_control_0, 'measurements'))    
+        self.msg_connect((self.gsm_receiver_0, 'C0'), (self.ts_filter, 'in'))    
         self.msg_connect((self.gsm_trx_burst_if_0, 'bursts'), (self.gsm_txtime_setter_0, 'bursts_in'))    
         self.msg_connect((self.gsm_txtime_setter_0, 'bursts_out'), (self.gsm_preprocess_tx_burst_0, 'bursts_in'))    
+        self.msg_connect((self.ts_filter, 'out'), (self.gsm_trx_burst_if_0, 'bursts'))    
         self.connect((self.blocks_pdu_to_tagged_stream_0_0, 0), (self.gsm_gmsk_mod_0, 0))    
         self.connect((self.digital_burst_shaper_xx_0, 0), (self.gsm_msg_to_tag_0_0, 0))    
         self.connect((self.gsm_controlled_rotator_cc_0, 0), (self.low_pass_filter_0_0, 0))    
@@ -116,40 +127,7 @@ class trx_radio_if(gr.top_block):
 
     def set_delay_correction(self, delay_correction):
         self.delay_correction = delay_correction
-
-    def get_fc_downlink(self):
-        return self.fc_downlink
-
-    def set_fc_downlink(self, fc_downlink):
-        self.fc_downlink = fc_downlink
-        self.gsm_controlled_rotator_cc_0.set_phase_inc(self.ppm/1.0e6*2*math.pi*self.fc_downlink/self.samp_rate)
-        self.gsm_controlled_rotator_cc_0_0.set_phase_inc(-self.ppm/1.0e6*2*math.pi*self.fc_downlink/self.samp_rate)
-        self.uhd_usrp_source_0.set_center_freq(self.fc_downlink, 0)
-        self.uhd_usrp_source_0.set_center_freq(uhd.tune_request_t(self.fc_downlink, self.fc_uplink-self.fc_downlink), 1)
-
-    def get_fc_uplink(self):
-        return self.fc_uplink
-
-    def set_fc_uplink(self, fc_uplink):
-        self.fc_uplink = fc_uplink
-        self.uhd_usrp_sink_0.set_center_freq(self.fc_uplink, 0)
-        self.uhd_usrp_source_0.set_center_freq(uhd.tune_request_t(self.fc_downlink, self.fc_uplink-self.fc_downlink), 1)
-
-    def get_gain_downlink(self):
-        return self.gain_downlink
-
-    def set_gain_downlink(self, gain_downlink):
-        self.gain_downlink = gain_downlink
-        self.uhd_usrp_source_0.set_gain(self.gain_downlink, 0)
-        	
-
-    def get_gain_uplink(self):
-        return self.gain_uplink
-
-    def set_gain_uplink(self, gain_uplink):
-        self.gain_uplink = gain_uplink
-        self.uhd_usrp_sink_0.set_gain(self.gain_uplink, 0)
-        	
+        self.gsm_txtime_setter_0.set_delay_correction(self.delay_correction+self.uplink_shift)
 
     def get_osr(self):
         return self.osr
@@ -163,31 +141,77 @@ class trx_radio_if(gr.top_block):
 
     def set_ppm(self, ppm):
         self.ppm = ppm
-        self.gsm_controlled_rotator_cc_0.set_phase_inc(self.ppm/1.0e6*2*math.pi*self.fc_downlink/self.samp_rate)
-        self.gsm_controlled_rotator_cc_0_0.set_phase_inc(-self.ppm/1.0e6*2*math.pi*self.fc_downlink/self.samp_rate)
+        self.gsm_controlled_rotator_cc_0.set_phase_inc(self.ppm/1.0e6*2*math.pi*self.rx_freq/self.samp_rate)
+        self.gsm_controlled_rotator_cc_0_0.set_phase_inc(-self.ppm/1.0e6*2*math.pi*self.tx_freq/self.samp_rate)
+
+    def get_rx_freq(self):
+        return self.rx_freq
+
+    def set_rx_freq(self, rx_freq):
+        self.rx_freq = rx_freq
+        self.gsm_controlled_rotator_cc_0.set_phase_inc(self.ppm/1.0e6*2*math.pi*self.rx_freq/self.samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(self.rx_freq, 0)
+
+    def get_rx_gain(self):
+        return self.rx_gain
+
+    def set_rx_gain(self, rx_gain):
+        self.rx_gain = rx_gain
+        self.uhd_usrp_source_0.set_gain(self.rx_gain, 0)
+        	
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.gsm_controlled_rotator_cc_0.set_phase_inc(self.ppm/1.0e6*2*math.pi*self.fc_downlink/self.samp_rate)
-        self.gsm_controlled_rotator_cc_0_0.set_phase_inc(-self.ppm/1.0e6*2*math.pi*self.fc_downlink/self.samp_rate)
+        self.gsm_controlled_rotator_cc_0.set_phase_inc(self.ppm/1.0e6*2*math.pi*self.rx_freq/self.samp_rate)
+        self.gsm_controlled_rotator_cc_0_0.set_phase_inc(-self.ppm/1.0e6*2*math.pi*self.tx_freq/self.samp_rate)
         self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, 125e3, 5e3, firdes.WIN_HAMMING, 6.76))
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
-
-    def get_samp_rate_downlink(self):
-        return self.samp_rate_downlink
-
-    def set_samp_rate_downlink(self, samp_rate_downlink):
-        self.samp_rate_downlink = samp_rate_downlink
 
     def get_timing_advance(self):
         return self.timing_advance
 
     def set_timing_advance(self, timing_advance):
         self.timing_advance = timing_advance
+        self.gsm_txtime_setter_0.set_timing_advance(self.timing_advance)
+
+    def get_trx_base_port(self):
+        return self.trx_base_port
+
+    def set_trx_base_port(self, trx_base_port):
+        self.trx_base_port = trx_base_port
+
+    def get_trx_remote_addr(self):
+        return self.trx_remote_addr
+
+    def set_trx_remote_addr(self, trx_remote_addr):
+        self.trx_remote_addr = trx_remote_addr
+
+    def get_tx_freq(self):
+        return self.tx_freq
+
+    def set_tx_freq(self, tx_freq):
+        self.tx_freq = tx_freq
+        self.gsm_controlled_rotator_cc_0_0.set_phase_inc(-self.ppm/1.0e6*2*math.pi*self.tx_freq/self.samp_rate)
+        self.uhd_usrp_sink_0.set_center_freq(self.tx_freq, 0)
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.uhd_usrp_sink_0.set_gain(self.tx_gain, 0)
+        	
+
+    def get_uplink_shift(self):
+        return self.uplink_shift
+
+    def set_uplink_shift(self, uplink_shift):
+        self.uplink_shift = uplink_shift
+        self.gsm_txtime_setter_0.set_delay_correction(self.delay_correction+self.uplink_shift)
 
 
 def argument_parser():
@@ -196,40 +220,46 @@ def argument_parser():
         "", "--delay-correction", dest="delay_correction", type="eng_float", default=eng_notation.num_to_str(285.616e-6),
         help="Set delay_correction [default=%default]")
     parser.add_option(
-        "-d", "--fc-downlink", dest="fc_downlink", type="eng_float", default=eng_notation.num_to_str(938600000),
-        help="Set fc_downlink [default=%default]")
-    parser.add_option(
-        "-u", "--fc-uplink", dest="fc_uplink", type="eng_float", default=eng_notation.num_to_str(938900000),
-        help="Set fc_uplink [default=%default]")
-    parser.add_option(
-        "-g", "--gain-downlink", dest="gain_downlink", type="eng_float", default=eng_notation.num_to_str(40),
-        help="Set gain_downlink [default=%default]")
-    parser.add_option(
-        "-r", "--gain-uplink", dest="gain_uplink", type="eng_float", default=eng_notation.num_to_str(60),
-        help="Set gain_uplink [default=%default]")
-    parser.add_option(
         "", "--osr", dest="osr", type="intx", default=4,
         help="Set OSR [default=%default]")
     parser.add_option(
-        "", "--ppm", dest="ppm", type="eng_float", default=eng_notation.num_to_str(-1),
+        "", "--ppm", dest="ppm", type="eng_float", default=eng_notation.num_to_str(-0.799427),
         help="Set Clock offset correction [default=%default]")
+    parser.add_option(
+        "-d", "--rx-freq", dest="rx_freq", type="eng_float", default=eng_notation.num_to_str(935e6+36*0.2e6),
+        help="Set rx_freq [default=%default]")
+    parser.add_option(
+        "-g", "--rx-gain", dest="rx_gain", type="eng_float", default=eng_notation.num_to_str(40),
+        help="Set rx_gain [default=%default]")
     parser.add_option(
         "", "--samp-rate", dest="samp_rate", type="eng_float", default=eng_notation.num_to_str(13e6/12.0),
         help="Set samp_rate [default=%default]")
     parser.add_option(
-        "", "--samp-rate-downlink", dest="samp_rate_downlink", type="eng_float", default=eng_notation.num_to_str(13e6/12.0),
-        help="Set samp_rate_downlink [default=%default]")
-    parser.add_option(
         "", "--timing-advance", dest="timing_advance", type="eng_float", default=eng_notation.num_to_str(0),
         help="Set timing_advance [default=%default]")
+    parser.add_option(
+        "", "--trx-base-port", dest="trx_base_port", type="string", default="5710",
+        help="Set 5710 [default=%default]")
+    parser.add_option(
+        "", "--trx-remote-addr", dest="trx_remote_addr", type="string", default="127.0.0.1",
+        help="Set 127.0.0.1 [default=%default]")
+    parser.add_option(
+        "-u", "--tx-freq", dest="tx_freq", type="eng_float", default=eng_notation.num_to_str(935e6+36*0.2e6-45e6),
+        help="Set tx_freq [default=%default]")
+    parser.add_option(
+        "-r", "--tx-gain", dest="tx_gain", type="eng_float", default=eng_notation.num_to_str(40),
+        help="Set tx_gain [default=%default]")
+    parser.add_option(
+        "", "--uplink-shift", dest="uplink_shift", type="eng_float", default=eng_notation.num_to_str(-(6.0/1625000*(156.25)*3)),
+        help="Set uplink_shift [default=%default]")
     return parser
 
 
-def main(top_block_cls=trx_radio_if, options=None):
+def main(top_block_cls=radio_if_grc, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    tb = top_block_cls(delay_correction=options.delay_correction, fc_downlink=options.fc_downlink, fc_uplink=options.fc_uplink, gain_downlink=options.gain_downlink, gain_uplink=options.gain_uplink, osr=options.osr, ppm=options.ppm, samp_rate=options.samp_rate, samp_rate_downlink=options.samp_rate_downlink, timing_advance=options.timing_advance)
+    tb = top_block_cls(delay_correction=options.delay_correction, osr=options.osr, ppm=options.ppm, rx_freq=options.rx_freq, rx_gain=options.rx_gain, samp_rate=options.samp_rate, timing_advance=options.timing_advance, trx_base_port=options.trx_base_port, trx_remote_addr=options.trx_remote_addr, tx_freq=options.tx_freq, tx_gain=options.tx_gain, uplink_shift=options.uplink_shift)
     tb.start()
     tb.wait()
 
